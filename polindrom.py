0d6459d85cdcbefb7ae8f38924c82dc3a1332955
korchalovsky <korchalovsky@gmail.com>
2020-03-16 01:32:30 +0300

    Add polindrom.py

---
+++ polindrom.py
@@ -0,0 +1,25 @@
+def get_palindrome(s, start, end):
+    while end + 1 < len(s) and s[end + 1] == s[start]:
+        end += 1
+
+    while start > 0 and end + 1 < len(s) and s[end + 1] == s[start - 1]:
+        start -= 1
+        end += 1
+
+    return s[start:end + 1]
+
+
+def longest_palindrome(str):
+    long = []
+    for i in range(len(str)):
+        candidate = get_palindrome(str, i, i)
+
+        if len(candidate) > len(long):
+            long = candidate
+
+    return print(long)
+
+
+str = 'ллолрарараорло'
+
+longest_palindrome(str)
