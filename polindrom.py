0993b8e51350fef408bfa551dda2e3e400d08bdb
korchalovsky <korchalovsky@gmail.com>
2020-03-16 12:55:00 +0300

    Made some changes

--- polindrom.py
+++ polindrom.py
@@ -1,18 +1,18 @@
-def get_palindrome(str, start, end):
-    while end + 1 < len(str) and str[end + 1] == str[start]:
+def get_palindrome(string, start, end):
+    while end + 1 < len(string) and string[end + 1] == string[start]:
         end += 1
 
-    while start > 0 and end + 1 < len(str) and str[end + 1] == str[start - 1]:
+    while start > 0 and end + 1 < len(string) and string[end + 1] == string[start - 1]:
         start -= 1
         end += 1
 
     return str[start:end + 1]
 
 
-def longest_palindrome(str):
+def longest_palindrome(string):
     long = []
-    for i in range(len(str)):
-        candidate = get_palindrome(str, i, i)
+    for i in range(len(string)):
+        candidate = get_palindrome(string, i, i)
 
         if len(candidate) > len(long):
             long = candidate
